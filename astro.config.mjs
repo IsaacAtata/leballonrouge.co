f228c50a29fd1e0c05dacf6a4e7c879434905061
Akram Atata <000886844@mohawkcollege.ca>
2026-03-23 09:16:15 -0400

    Final Dev version of Le Ballon Rouge

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,20 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import tunnel from "astro-tunnel";
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+
   i18n: {
     defaultLocale: "fr",
     locales: ["fr", "en"],
     routing: {
       prefixDefaultLocale: false
     }
-  }
+  },
+
+  integrations: [tunnel()]
 });
